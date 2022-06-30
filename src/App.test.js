ff15720096ce9d87451b6b2ac5265a915eb2d324
Tayná Ramos <tayna.ramos@ifood.com.br>
2022-06-30 11:46:56 -0300

    isolar o render do componente

--- src/App.test.js
+++ src/App.test.js
@@ -58,14 +58,35 @@ describe('Componente principal', () => {
             expect(novoSaldo < 0).toBe(false)
         })
 
+        // it('que é um saque, a transação deve ser realizada', () => {
+
+        //     const { getByText, getByTestId, getByLabelText } = render(<App />);
+
+        //     const saldo = getByText('R$ 1000');
+        //     const transacao = getByLabelText('Saque');
+        //     const valor = getByTestId('valor');
+        //     const botaoTransacao = getByText('Realizar operação');
+
+
+        //     expect(saldo.textContent).toBe('R$ 1000')
+            
+        //     // simula eventos
+        //     fireEvent.click(transacao, { target: { value: 'saque' } })
+        //     fireEvent.change(valor, { target: { value: 10 } })
+        //     fireEvent.click(botaoTransacao)
+
+        //     expect(saldo.textContent).toBe('R$ 990')
+
+        // })
+
         it('que é um saque, a transação deve ser realizada', () => {
 
-            const { getByText, getByTestId, getByLabelText } = render(<App />);
+            render(<App />);
 
-            const saldo = getByText('R$ 1000');
-            const transacao = getByLabelText('Saque');
-            const valor = getByTestId('valor');
-            const botaoTransacao = getByText('Realizar operação');
+            const saldo = screen.getByText('R$ 1000');
+            const transacao = screen.getByLabelText('Saque');
+            const valor = screen.getByTestId('valor');
+            const botaoTransacao = screen.getByText('Realizar operação');
 
 
             expect(saldo.textContent).toBe('R$ 1000')
