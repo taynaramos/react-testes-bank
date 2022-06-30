e59099ce20d1108a7a12a8fe783ab3f5ccc26fac
Tayná Ramos <tayna.ramos@ifood.com.br>
2022-06-30 12:35:34 -0300

    Simulando uma função

--- src/conta/Conta.test.js
+++ src/conta/Conta.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Conta from './Conta'
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 describe('Componente de conta', () => {
     it('Exibir o saldo da conta com formatação monetária', () => {
@@ -9,5 +9,15 @@ describe('Componente de conta', () => {
         const saldo = screen.getByTestId('saldo-conta')
 
         expect(saldo.textContent).toBe('R$ 1000');
+    })
+
+    it('Chama a função de realizar transação, quando o botão é clicado', () => {
+        // mock de funcao: função que não faz nada, mas que conseguimos checar pelo nome dela se ela foi chamada no nosso teste.
+        const funcaoRealizarTransacao = jest.fn()
+
+        render(<Conta saldo={1000} realizarTransacao={funcaoRealizarTransacao} />) 
+
+        fireEvent.click(screen.getByText('Realizar operação'))
+        expect(funcaoRealizarTransacao).toHaveBeenCalled()
     })
 })
